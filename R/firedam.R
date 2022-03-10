dd749df5407513638a8c156a94d221bee03ef72f
Reagan Rasmussen <reagan@MacBook-Pro-4.local>
2022-03-10 15:57:25 -0600

    Commit for Proj1

---
+++ R/firedam.R
@@ -0,0 +1,12 @@
+#' Fire damage data set.
+#'
+#' 15 lines of measurements on property damage due to fire
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance of property from damage, in miles}
+#'   \item{DAMAGE}{damage done due to the fire, in thousands of dollars}
+#'   ...
+#' }
+#' @source Textbook
+"firedam"
